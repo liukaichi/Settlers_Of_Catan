6a9ffc2a0108ff14132fe3d1a04a85a1d2841965
David Taylor <David.Taylor@wavetronix.com>
2015-09-15 09:53:26 -0600

    fixed javadoc

--- java/src/server/proxy/IProxy.java
+++ java/src/server/proxy/IProxy.java
@@ -31,26 +31,32 @@ public interface IProxy {
 	List<GameInfo> listGames();
 	
 	/** Adds an AI player to the current game.
-	 * 
-	 * On success:
-	 * 1. The server returns an HTTP 200 success response with “Success” in the body.
-	 * 2. A new AI player of the specified type has been added to the current game. The server selected a name and color for the player.
-	 * On failure:
-	 * 1. The server returns an HTTP 400 error response, and the body contains an error message.
-	 * 
+	 * <p>
+	 * <b>On success:</b>
+	 * <ul>
+	 * <li>1. The server returns an HTTP 200 success response with “Success” in the body.
+	 * <li>2. A new AI player of the specified type has been added to the current game. The server selected a name and color for the player.
+	 * </ul>
+	 * <b>On failure:</b>
+	 * <ul>
+	 * <li>1. The server returns an HTTP 400 error response, and the body contains an error message.
+	 * </ul>
 	 * @param aiType Values returned by listAI, currently only LARGEST_ARMY is supported
 	 */
 	void addAI(AIType aiType);
 	
 	//Util Method
 	/** Sets the server’s logging level.
-	 * 
-	 * On success:
-	 * 1. The server returns an HTTP 200 success response with “Success” in the body.
-	 * 2. The Server is using the specified logging level
-	 * On failure:
-	 * 1. The server returns an HTTP 400 error response, and the body contains an error message
-	 * 
+	 * <p>
+	 * <b>On success:</b>
+	 * <ul>
+	 * <li>1. The server returns an HTTP 200 success response with “Success” in the body.
+	 * <li>2. The Server is using the specified logging level
+	 * </ul>
+	 * <b>On failure:</b>
+	 * <ul>
+	 * <li>1. The server returns an HTTP 400 error response, and the body contains an error message
+	 * </ul>
 	 * @param level Valid values include: SEVERE, WARNING, INFO, CONFIG, FINE, FINER, FINEST
 	 */
 	void changeLogLevel(Level level);
