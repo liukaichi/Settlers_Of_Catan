073fd329eb0375492b4d3c1791f18effd5ae68ea
Cache Staheli <cache117@gmail.com>
2015-09-14 23:34:16 -0600

    Added params/return types.

--- java/src/server/proxy/IProxy.java
+++ java/src/server/proxy/IProxy.java
@@ -1,14 +1,26 @@
 package server.proxy;
 
+import java.util.List;
+
+import client.data.*;
+import shared.model.*;
+
+
 public interface IProxy {
 
 	String method();
 	
-	void userLogin();
+	//User Methods
+	void userLogin(UserParam userParameter); //Or just Username/Password. We should discuss this, probably.
 	
-	void userRegister();
+	void userRegister(UserParam userParameter);
 	
-	void listGames();
+	//Game Methods
+	List<GameInfo> listGames();
+	
+	//Util Method
+	
+	//Move Methods
 	
 	
 }
