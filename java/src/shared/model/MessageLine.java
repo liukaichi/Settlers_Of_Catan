b04b68d29a654865ca87f599b287570c32fcc06a
Adrian Foong <adrian_foong@hotmail.com>
2015-09-16 12:13:14 -0600

    Adrian Test

--- java/src/shared/model/MessageLine.java
+++ java/src/shared/model/MessageLine.java
@@ -1,6 +1,18 @@
 package shared.model;
 
+/**
+ * Represents a single message line in the chat.
+ */
 public class MessageLine
 {
-    private String message, source;
+    /**
+     * The text body of the message.
+     */
+    private String message;
+    
+    /**
+     * The source of the message.
+     * The source can be a player or the server.
+     */
+    private String source;
 }
--- java/src/shared/model/Player.java
+++ java/src/shared/model/Player.java
@@ -2,9 +2,20 @@ package shared.model;
 
 import client.data.PlayerInfo;
 
+/**
+ * Represents a player playing the game.
+ * There can be up to 4 players in a single game.
+ */
 public class Player
 {
+    /**
+     * @see Bank
+     */
     private Bank bank;
+    
+    /**
+     * 
+     */
     private PlayerInfo playerInfo;
     // probably should create a Name class?
     private String name;
