b3aa96f8e01ad3fcd0ac478ea3ddbed943550789
liukaichi <liukaichi@Gmail.com>
2015-09-16 16:59:24 -0600

    Made a simple Poller and IPoller in the Client folder. Javadoc yet to be completed

---
+++ java/src/client/poller/IPoller.java
@@ -0,0 +1,27 @@
+package client.poller;
+
+import server.proxy.IProxy;
+import shared.model.ClientModel;
+
+/**
+ *
+ The Server Poller should be fairly simple to design and implement. It should call the
+ Server Proxy to retrieve the current model state, and then update the local model’s
+ state with the returned JSON data.
+ Design your Server Poller to use dependency injection so that it can be easily
+ configured to use either the “real” or “mock” Server Proxy implementation. This
+ means that rather than calling “new” internally to create a proxy object, the poller
+ should instead have a constructor parameter or setter that can be used to pass in the
+ proxy it should use
+ */
+public interface IPoller {
+
+
+
+    void setProxy(IProxy proxy);
+
+    void poll(IProxy proxy);
+
+    void updateModel(ClientModel newModel);
+
+}
