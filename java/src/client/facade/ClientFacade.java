dc24a34971c5815318ad6753baea14e4b4023cf9
Cache Staheli <cache117@gmail.com>
2015-09-23 09:18:16 -0600

    Facade, multiple exceptions, Server/MockProxy.
    Wrote the methods for the Facade, made those throw some new custom
    exceptions, and wrote the classes (ServerProxy and MockProxy) to
    implement IProxy.

---
+++ java/src/client/facade/ClientFacade.java
@@ -0,0 +1,266 @@
+package client.facade;
+
+import client.data.RobPlayerInfo;
+import shared.definitions.ResourceType;
+import shared.definitions.exceptions.*;
+import shared.locations.*;
+import shared.model.ClientModel;
+import shared.model.resource.ResourceList;
+
+public class ClientFacade
+{
+    private static ClientFacade _instance = null;
+    private ClientModel model;
+
+    private ClientFacade()
+    {
+        model = new ClientModel();
+    }
+
+    public static ClientFacade getInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new ClientFacade();
+        }
+        return _instance;
+    }
+
+    /*
+     * Chat Controller methods
+     */
+    public void sendMessage(String message) throws CatanException
+    {
+
+    }
+
+    /*
+     * Game History Controller
+     */
+    public void initHistoryFromModel() throws CatanException
+    {
+
+    }
+
+    /*
+     * Dev Card Controller Methods
+     */
+
+    public void buyCard() throws CardException
+    {
+
+    }
+
+    public void playMonopolyCard(ResourceType resource) throws CardException
+    {
+
+    }
+
+    public void playMonumentCard() throws CardException
+    {
+
+    }
+
+    public void playRoadBuildCard() throws CardException
+    {
+
+    }
+
+    public void playSoldierCard() throws CardException
+    {
+
+    }
+
+    public void playYearOfPlentyCard(ResourceType resource1, ResourceType resource2) throws CardException
+    {
+
+    }
+
+    /*
+     * Discard Controller methods
+     */
+
+    public void increaseResourceAmountDiscarded(ResourceType resource) throws DiscardException
+    {
+
+    }
+
+    public void decreaseResourceAmountDiscarded(ResourceType resource) throws DiscardException
+    {
+
+    }
+
+    public void discardResources(ResourceList discardedResources) throws DiscardException
+    {
+
+    }
+
+    /*
+     * Domestic trade controller methods
+     */
+
+    public void decreaseTradeResourceAmount(ResourceType resource) throws TradingException
+    {
+
+    }
+
+    public void increaseTradeResourceAmount(ResourceType resource) throws TradingException
+    {
+
+    }
+
+    public void sendTradeOffer() throws TradingException
+    {
+    }
+
+    public void setPlayerToTradeWith(int playerIndex) throws TradingException
+    {
+
+    }
+
+    public void setResourceToReceive(ResourceType resource) throws TradingException
+    {
+
+    }
+
+    public void setResourceToSend(ResourceType resource) throws TradingException
+    {
+
+    }
+
+    public void unsetTradeResource(ResourceType resource) throws TradingException
+    {
+
+    }
+
+    public void cancelTrade() throws TradingException
+    {
+
+    }
+
+    public void acceptTrade(boolean willAccept) throws TradingException
+    {
+
+    }
+
+    /*
+     * Join Game Controller methods
+     */
+    public void createNewGame() throws CatanException
+    {
+
+    }
+
+    public void joinGame() throws CatanException
+    {
+
+    }
+
+    /*
+     * Player Waiting Controller methods
+     */
+
+    public void addAI() throws CatanException
+    {
+
+    }
+
+    /*
+     * Login Controller methods
+     */
+
+    public void signInUser() throws SignInException
+    {
+
+    }
+
+    public void registerUser() throws SignInException
+    {
+
+    }
+
+    /*
+     * Map controller methods
+     */
+    public boolean canPlaceRoad(EdgeLocation edgeLoc)
+    {
+        return false;
+    }
+
+    public boolean canPlaceSettlement(VertexLocation vertLoc)
+    {
+        return false;
+    }
+
+    public boolean canPlaceCity(VertexLocation vertLoc)
+    {
+        return false;
+    }
+
+    public boolean canPlaceRobber(HexLocation hexLoc)
+    {
+        return false;
+    }
+
+    public void placeRoad(EdgeLocation edgeLoc) throws PlacementException
+    {
+
+    }
+
+    public void placeSettlement(VertexLocation vertLoc) throws PlacementException
+    {
+
+    }
+
+    public void placeCity(VertexLocation vertLoc) throws PlacementException
+    {
+
+    }
+
+    public void placeRobber(HexLocation hexLoc) throws PlacementException
+    {
+
+    }
+
+    public void robPlayer(RobPlayerInfo victim) throws PlacementException
+    {
+
+    }
+
+    /*
+     * Martitime Trade Controller methods
+     */
+    public void makeTrade() throws TradingException
+    {
+
+    }
+
+    /*
+     * Points Controller methods
+     */
+    public void initPointsFromModel()
+    {
+
+    }
+
+    /*
+     * Roll Dice Controller methods
+     */
+    public void rollDice()
+    {
+
+    }
+
+    /*
+     * Turn tracker controller methods
+     */
+    public void endTurn()
+    {
+
+    }
+
+    public void initTurnFromModel()
+    {
+
+    }
+}
---
+++ java/src/server/proxy/ServerProxy.java
@@ -0,0 +1,256 @@
+package server.proxy;
+
+import java.util.List;
+import java.util.logging.Level;
+
+import client.data.GameInfo;
+import shared.communication.*;
+import shared.communication.moveCommands.*;
+import shared.model.ClientModel;
+import shared.model.player.Credentials;
+
+public class ServerProxy implements IProxy
+{
+
+    @Override
+    public void userLogin(Credentials credentials)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void userRegister(Credentials credentials)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void changeLogLevel(Level level)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public List<GameInfo> listGames()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public CreateGameResponse createGame(CreateGameRequest createGameRequest)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void joinGame(JoinGameRequest joinGameRequest)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void saveGame(SaveGameRequest saveGameRequest)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void loadGame(LoadGameRequest loadGameRequest)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public ClientModel getGameState(int versionNumber)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ClientModel resetGame()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<Command> getCommands()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ClientModel postCommands(List<Command> commands)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<AIType> listAI()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void addAI(AIType aiType)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public ClientModel sendChat(SendChatCommand sendChat)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ClientModel rollNumber(RollNumberCommand rollNumber)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ClientModel acceptTrade(AcceptTradeCommand acceptTrade)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ClientModel discardCards(DiscardCardsCommand discardCards)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ClientModel buildRoad(BuildRoadCommand buildRoad)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ClientModel buildSettlement(BuildSettlementCommand buildSettlement)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ClientModel buildCity(BuildCityCommand buildCity)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ClientModel offerTrade(OfferTradeCommand offerTrade)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ClientModel maritimeTrade(MaritimeTradeCommand maritimeTrade)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ClientModel robPlayer(RobPlayerCommand robPlayer)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ClientModel finishTurn(FinishTurnCommand finishTurn)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ClientModel buyDevCard(BuyDevCardCommand buyDevCard)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ClientModel soldier(SoldierCommand soldier)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ClientModel yearOfPlenty(YearOfPlentyCommand yearOfPlenty)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ClientModel roadBuilding(RoadBuildingCommand roadBuilding)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ClientModel monopoly(MonopolyCommand monopoly)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ClientModel monument(MonumentCommand monument)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /**
+     * Takes an object and turns it into JSON.
+     * 
+     * @param object
+     *        the object to serialize
+     * @return The Formatted JSON from the object.
+     */
+    private String serializeClass(Object object)
+    {
+        return null;
+    }
+
+    /**
+     * Takes JSON and turns it into an object
+     * 
+     * @param json
+     *        the JSON to deserialize.
+     * @return the Object from the JSON.
+     */
+    private Object deserializeJSON(String json)
+    {
+        return null;
+    }
+
+}
