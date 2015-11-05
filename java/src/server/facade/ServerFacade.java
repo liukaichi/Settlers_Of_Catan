493c95bfb60b94f7d330fa023756e6cbe9210901
Adrian Foong <adrian_foong@hotmail.com>
2015-11-04 23:49:17 -0700

    JavaDocs for ServerFacade

--- java/src/server/facade/ServerFacade.java
+++ java/src/server/facade/ServerFacade.java
@@ -64,60 +64,121 @@ public class ServerFacade extends AbstractServerFacade
         return null;
     }
 
+    /**
+     * Add a random Development Card to the client player's PlayerBank
+     * Pop said Development Card from the DevCardDeck
+     * Display bought Development Card message.
+     * @return the updated ClientModel
+     */
     @Override public ClientModel buyDevCard()
     {
         model.buyDevCard();
         return null;
     }
 
+    /**
+     * Increase up to two resources to the client player's PlayerBank
+     * Decrease the appropriate resources from the game bank.
+     * Display resources received message.
+     * @return the updated ClientModel
+     */
     @Override public ClientModel yearOfPlenty()
     {
         return null;
     }
 
+    /**
+     * Increase the client player's roads of AmountType.BUILT by 2
+     * Decrease the client player's roads of AmountType.AVAILABLE by 2
+     * Update the map with the 2 new roads
+     * Display 2 roads built message
+     * @return the updated ClientModel
+     */
     @Override public ClientModel roadBuilding()
     {
         return null;
     }
 
+    /**
+     * Update the map with the new Robber location.
+     * Increase the client player's PlayerBank with the robbed resource.
+     * Decrease the robber player's PlayerBank with the robber resource.
+     * Display robbed message.
+     * @return the updated ClientModel
+     */
     @Override public ClientModel soldier()
     {
         return null;
     }
 
+    /**
+     * Updates the client player's PlayerBank with the resources gained.
+     * Updates all other players' PlayerBank with the resources lost.
+     * @return the updated ClientModel
+     */
     @Override public ClientModel monopoly()
     {
         return null;
     }
 
+    /**
+     * Increase the client player's Victory Points by 1
+     * @return the updated ClientModel
+     */
     @Override public ClientModel monument()
     {
         return null;
     }
 
+    /**
+     * Increase the client player's road of AmountType.BUILT by 1.
+     * Decrease the client player's road of AmountType.AVAILABLE by 1.
+     * Update the map with a new road.
+     * @return the updated ClientModel
+     */
     @Override public ClientModel buildRoad()
     {
         model.buildRoad(null, null);
         return null;
     }
 
+    /**
+     * Increase the client player's settlement of AmountType.BUILT by 1.
+     * Decrease the client player's settlement of AmountType.AVAILABLE by 1.
+     * Update the map with a new settlement
+     * @return the updated ClientModel
+     */
     @Override public ClientModel buildSettlement()
     {
         model.buildSettlement(null, null);
         return null;
     }
 
+    /**
+     * Increase the client player's city of AmountType.BUILT by 1.
+     * Decrease the client player's city of AmountType.AVAILABLE by 1.
+     * Update the map with a new city
+     * @return the updated ClientModel
+     */
     @Override public ClientModel buildCity()
     {
         model.buildCity(null, null);
         return null;
     }
 
+    /**
+     * Update model with a new TradeOffer object
+     * @return the updated ClientModel
+     */
     @Override public ClientModel offerTrade()
     {
         return null;
     }
 
+    /**
+     *
+     * @return
+     */
     @Override public ClientModel acceptTrade()
     {
         return null;
