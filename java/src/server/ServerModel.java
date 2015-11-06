1baaf200da68879e88581d5a258d28a418487a7e
Cache Staheli <cache117@gmail.com>
2015-11-05 23:32:14 -0700

    ServerModel

--- java/src/server/ServerModel.java
+++ java/src/server/ServerModel.java
@@ -1,5 +1,6 @@
 package server;
 
+import server.facade.IMovesFacade;
 import shared.definitions.PlayerIndex;
 import shared.definitions.ResourceType;
 import shared.definitions.TradeRatio;
@@ -7,54 +8,55 @@ import shared.locations.EdgeLocation;
 import shared.locations.HexLocation;
 import shared.locations.VertexLocation;
 import shared.model.ClientModel;
+import shared.model.bank.resource.Resources;
 import shared.model.player.TradeOffer;
 
 /**
  * Created by cstaheli on 11/5/2015.
  */
-public class ServerModel extends ClientModel
+public class ServerModel extends ClientModel implements IMovesFacade
 {
-    public ClientModel sendChat(PlayerIndex playerIndex, String content)
+    @Override public ClientModel sendChat(PlayerIndex playerIndex, String content)
     {
         return null;
     }
-    public ClientModel rollNumber(PlayerIndex playerIndex, int number)
+    @Override public ClientModel rollNumber(PlayerIndex playerIndex, int number)
     {
         return null;
     }
-    public ClientModel robPlayer(PlayerIndex playerIndex, PlayerIndex victim, HexLocation location)
+    @Override public ClientModel robPlayer(PlayerIndex playerIndex, PlayerIndex victim, HexLocation location)
     {
         return null;
     }
-    public ClientModel finishTurn(PlayerIndex playerIndex)
+    @Override public ClientModel finishTurn(PlayerIndex playerIndex)
     {
         return null;
     }
-    public ClientModel buyDevCard(PlayerIndex playerIndex)
+    @Override public ClientModel buyDevCard(PlayerIndex playerIndex)
     {
         return null;
     }
-    public ClientModel yearOfPlenty(PlayerIndex playerIndex, ResourceType resource1, ResourceType resource2)
+    @Override public ClientModel yearOfPlenty(PlayerIndex playerIndex, ResourceType resource1, ResourceType resource2)
     {
         return null;
     }
-    public ClientModel roadBuilding(PlayerIndex playerIndex, EdgeLocation spot1, EdgeLocation spot2)
+    @Override public ClientModel roadBuilding(PlayerIndex playerIndex, EdgeLocation spot1, EdgeLocation spot2)
     {
         return null;
     }
-    public ClientModel soldier(PlayerIndex playerIndex, PlayerIndex victimIndex, HexLocation location)
+    @Override public ClientModel soldier(PlayerIndex playerIndex, PlayerIndex victimIndex, HexLocation location)
     {
         return null;
     }
-    public ClientModel monopoly(PlayerIndex playerIndex, ResourceType resource)
+    @Override public ClientModel monopoly(PlayerIndex playerIndex, ResourceType resource)
     {
         return null;
     }
-    public ClientModel monument(PlayerIndex playerIndex)
+    @Override public ClientModel monument(PlayerIndex playerIndex)
     {
         return null;
     }
-    public ClientModel buildRoad(PlayerIndex playerIndex, EdgeLocation roadLocation, boolean free)
+    @Override public ClientModel buildRoad(PlayerIndex playerIndex, EdgeLocation roadLocation, boolean free)
     {
         return null;
     }
@@ -63,33 +65,35 @@ public class ServerModel extends ClientModel
      * @param player the player who is building the settlement.
      * @param location the location where the settlement is being built.
      */
-    public void buildSettlement(PlayerIndex player, VertexLocation location, boolean isFree)
+    @Override public ClientModel buildSettlement(PlayerIndex player, VertexLocation location, boolean isFree)
     {
-
+        return null;
     }
     /**
      * Builds a city for the given player at the given location.
      * @param player the player who is building the city.
      * @param location the location where the city is being built.
      */
-    public void buildCity(PlayerIndex player, VertexLocation location)
+    @Override public ClientModel buildCity(PlayerIndex player, VertexLocation location)
     {
         getMap().buildCity(player, location);
+        return null;
     }
-    public ClientModel offerTrade(PlayerIndex playerIndex, TradeOffer offer, PlayerIndex receiver)
+    @Override public ClientModel offerTrade(PlayerIndex playerIndex, TradeOffer offer, PlayerIndex receiver)
     {
         return null;
     }
-    public ClientModel acceptTrade(PlayerIndex playerIndex, boolean willAccept)
+    @Override public ClientModel acceptTrade(PlayerIndex playerIndex, boolean willAccept)
     {
         return null;
     }
-    public ClientModel maritimeTrade(PlayerIndex playerIndex, TradeRatio ratio, ResourceType inputResource,
+    @Override public ClientModel maritimeTrade(PlayerIndex playerIndex, TradeRatio ratio, ResourceType inputResource,
             ResourceType outputResource)
     {
         return null;
     }
-    public ClientModel discardCards()
+
+    @Override public ClientModel discardCards(PlayerIndex playerIndex, Resources discardedCards)
     {
         return null;
     }
