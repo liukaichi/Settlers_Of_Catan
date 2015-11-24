581d897de6879468f82ce5dfa07c8ee7aa8a0c5a
Adrian Foong <adrian_foong@hotmail.com>
2015-11-23 23:03:22 -0700

    Rolling gives you resources

--- java/src/shared/model/map/CatanMap.java
+++ java/src/shared/model/map/CatanMap.java
@@ -930,7 +930,7 @@ public class CatanMap {
         List<PlayerIndex> players = new ArrayList<>();
         for (VertexLocation location : hexes.get(hexLocation).getVertices()) {
 
-            MapStructure structure = structures.get(location);
+            MapStructure structure = structures.get(location.getNormalizedLocation());
             if (structure != null && structure instanceof City) {
                 players.add(structure.getOwner());
             }
