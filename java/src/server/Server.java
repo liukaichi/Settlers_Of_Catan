901f00e2bd9d46b3253757943d48a333c9c67b3e
David Taylor <David.Taylor@wavetronix.com>
2015-12-02 11:57:49 -0700

    cleaned up server and main

--- java/src/server/Server.java
+++ java/src/server/Server.java
@@ -22,85 +22,42 @@ public class Server
 {
 
     private static final int MAX_WAITING_CONNECTIONS = 10;
-    private static int DEFAULT_PORT_NUMBER = 8081;
-    private static String DEFAULT_HOST_NAME = "localhost";
-    private int checkpoints = 10;
-    private String persistenceType = "sqlite";
+    private static int PORT_NUMBER = 8081;
+    private static String HOST_NAME = "0.0.0.0";
+    private static int CHECKPOINTS = 10;
+    private static String PERSISTENCE_TYPE = "sqlite";
     private HttpServer server;
     private Logger LOGGER = Logger.getLogger(this.getClass().getName());
 
-    public Server(int server)
+    public void createContexts()
     {
-        DEFAULT_PORT_NUMBER = server;
-        AbstractServerFacade.setFacade(new ServerFacade());
-    }
-
-    public Server(String address, int port)
-    {
-        try
-        {
-            setPersistence();
-            AbstractServerFacade.setFacade(new ServerFacade());
-            //TODO: Change this to accept the port instead of the DEFAULT PORT NUMBER
-            server = HttpServer.create(new InetSocketAddress(address, port), 0);
             server.createContext("/game/", new GameHandler());
             server.createContext("/games/", new GamesHandler());
             server.createContext("/moves/", new MovesHandler());
             server.createContext("/user/", new UserHandler());
-
             server.createContext("/docs/api/data", new SwaggerHandler.JSONAppender(""));
             server.createContext("/docs/api/view", new SwaggerHandler.BasicFile(""));
+    }
 
+    public Server()
+    {
+        try
+        {
+            server = HttpServer.create(new InetSocketAddress(HOST_NAME, PORT_NUMBER), 0);
+            createContexts();
             server.setExecutor(null); // creates a default executor
+            AbstractServerFacade.setFacade(new ServerFacade());
+            IPersistenceFactory factory = PluginManager.createFactory(PERSISTENCE_TYPE);
+            UserManager.getInstance().setUserPersistence(factory.createUserPersistenceEngine());
+            GameManager.getInstance().setGamePersistence(factory.createGamePersistenceEngine(CHECKPOINTS));
         } catch (Exception e)
         {
             e.printStackTrace();
         }
-
-    }
-
-    public void setPersistence()
-    {
-        try
-        {
-            IPersistenceFactory factory = PluginManager.createFactory(persistenceType);
-            UserManager.getInstance().setUserPersistence(factory.createUserPersistenceEngine());
-            GameManager.getInstance().setGamePersistence(factory.createGamePersistenceEngine(checkpoints));
-        } catch (FileNotFoundException e)
-        {
-            e.printStackTrace();
-        }
-    }
-
-    public Server(String s, int port, String persistenceType, int checkpoints)
-    {
-        this(s,port,persistenceType);
-        this.checkpoints = checkpoints;
-        setPersistence();
-    }
-
-    public Server(String s, int port, String persistenceType)
-    {
-        this(s,port);
-        this.persistenceType = persistenceType;
-        setPersistence();
-    }
-
-    public static Server run()
-    {
-        return run(DEFAULT_PORT_NUMBER);
-    }
-    public static Server run(int port)
-    {
-        Server server = null;
-            server = new Server("0.0.0.0", port);
-            //DEFAULT_HOST_NAME = InetAddress.getLocalHost().getHostName();
-        server.start();
-        return server;
     }
     public void start()
     {
-        LOGGER.info("Starting Server on Host:"+DEFAULT_HOST_NAME +" Port:"+DEFAULT_PORT_NUMBER);
+        LOGGER.info("Starting Server on Host:"+ HOST_NAME +" Port:"+ PORT_NUMBER);
         try
         {
             LOGGER.info("Address: "+InetAddress.getLocalHost());
@@ -116,47 +73,56 @@ public class Server
         server.stop(0);
     }
 
-    public static void main(String[] args) {
-        //AbstractServerFacade.setFacade(new MockServerFacade());
-        AbstractServerFacade.setFacade(new ServerFacade());
-        switch(args.length) {
-            case 0:
-                Server.run();
-                break;
-            case 1://port
-                int port = Integer.parseInt(args[0]);
-                Server.run(port);
-                break;
-            case 2://port,persistence-type
-                port = Integer.parseInt(args[0]);
-                String persistenceType = args[1];
-                Server.run(port, persistenceType);
-                break;
-            case 3:
-                port = Integer.parseInt(args[0]);
-                persistenceType = args[1];
-                int checkpoints = Integer.parseInt(args[2]);
-                AbstractServerFacade.setFacade(new ServerFacade());
-                Server.run(port, persistenceType, checkpoints);
-                break;
-        }
-    }
-
     private static Server run(int port, String persistenceType, int checkpoints)
     {
-        Server server = null;
-        server = new Server("0.0.0.0", port, persistenceType, checkpoints);
-        //DEFAULT_HOST_NAME = InetAddress.getLocalHost().getHostName();
-        server.start();
-        return server;
+        CHECKPOINTS = checkpoints;
+        return run(port, persistenceType);
     }
 
     private static Server run(int port, String persistenceType)
     {
-        Server server = null;
-        server = new Server("0.0.0.0", port, persistenceType);
-        //DEFAULT_HOST_NAME = InetAddress.getLocalHost().getHostName();
+        PERSISTENCE_TYPE = persistenceType;
+        return run(port);
+    }
+
+    private static Server run(int port)
+    {
+        PORT_NUMBER = port;
+        return run();
+    }
+
+    public static Server run()
+    {
+        Server server = new Server();
         server.start();
         return server;
+    }
+
+    public static void main(String[] args) {
+        int port;
+        String persistenceType;
+        int checkpoints;
+        //AbstractServerFacade.setFacade(new MockServerFacade());
+        AbstractServerFacade.setFacade(new ServerFacade());
+        switch(args.length) {
+        case 0:
+            Server.run();
+            break;
+        case 1://port
+            port = Integer.parseInt(args[0]);
+            Server.run(port);
+            break;
+        case 2://port,persistence-type
+            port = Integer.parseInt(args[0]);
+            persistenceType = args[1];
+            Server.run(port, persistenceType);
+            break;
+        case 3:
+            port = Integer.parseInt(args[0]);
+            persistenceType = args[1];
+            checkpoints = Integer.parseInt(args[2]);
+            Server.run(port, persistenceType, checkpoints);
+            break;
+        }
     }
 }
