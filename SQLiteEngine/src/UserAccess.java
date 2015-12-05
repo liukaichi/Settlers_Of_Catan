b514f710d367d765ca608c0a6fdb3567f24749d7
liukaichi <liukaichi@Gmail.com>
2015-12-05 12:39:51 -0700

    added SQLiteEngine

--- SQLiteEngine/src/UserAccess.java
+++ SQLiteEngine/src/UserAccess.java
@@ -2,6 +2,9 @@ import server.manager.User;
 import server.plugin.IUserAccess;
 import shared.communication.Credentials;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 /**
  * SQL Database Access Object for Users.
  */
@@ -15,7 +18,26 @@ public class UserAccess implements IUserAccess
 
     public User getUser(Credentials credentials) throws Exception
     {
-        return null;
+        User result = null;
+        PreparedStatement stmt;
+        ResultSet rs;
+        String query = "SELECT UserID FROM User WHERE Name = "
+                + credentials.getUsername()
+                + " AND UserID = "
+                + credentials.getPassword();
+        stmt = engine.getConnection().prepareStatement(query);
+
+        rs = stmt.executeQuery();
+        if (!rs.isBeforeFirst()){
+            return null;
+        }
+        if (rs.next()) {
+            int id = rs.getInt(1);
+
+            result = new User(credentials, id);
+        }
+
+        return result;
     }
 
     public User getUser(int id) throws Exception
