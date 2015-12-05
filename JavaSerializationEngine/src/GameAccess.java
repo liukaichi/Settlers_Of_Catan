d8dc7da33c320d10ec2b6842aeafa973a0d20f7f
David Taylor <David.Taylor@wavetronix.com>
2015-12-05 15:36:28 -0700

    updated

--- JavaSerializationEngine/src/GameAccess.java
+++ JavaSerializationEngine/src/GameAccess.java
@@ -1,21 +1,33 @@
 import server.ServerModel;
 import server.plugin.IGameAccess;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by dtaylor on 12/5/2015.
  */
 public class GameAccess implements IGameAccess, IAccess
 {
+    Map<Integer, ServerModel> models;
     @Override public void initialize()
     {
-
+        models = new HashMap<>();
     }
 
     @Override public void updateModel(int gameID, ServerModel game) throws Exception
     {
-
+        ServerModel model = models.get(gameID);
+        if(model != null)
+        {
+            models.put(gameID, game);
+        }
+        else
+        {
+            models.put(gameID, game);
+        }
     }
 
     @Override public void addCommand(int gameID) throws Exception
