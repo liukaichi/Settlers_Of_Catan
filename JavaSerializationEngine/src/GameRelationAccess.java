d0c0a7e7640be03cffc515e6d57e53bfba360b5f
Amanda <theshecoder@gmail.com>
2015-12-05 16:08:13 -0700

    GameRelation Java Serialization Engine

--- JavaSerializationEngine/src/GameRelationAccess.java
+++ JavaSerializationEngine/src/GameRelationAccess.java
@@ -1,24 +1,43 @@
 import server.plugin.IGameRelationAccess;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by dtaylor on 12/5/2015.
  */
 public class GameRelationAccess implements IGameRelationAccess, IAccess
 {
+    Map<Integer, List<Integer>> users;
+
     @Override public void initialize()
     {
-
+        users = new HashMap<>();
     }
 
-    @Override public void addUserToGame(int userID, int gameID) throws Exception
+    @Override
+    public void addUserToGame(int gameID, int userID) throws Exception
     {
-
+        List<Integer> gameUsers = users.get(gameID);
+        if (gameUsers != null) {
+            gameUsers.add(userID);
+        } else {
+            users.put(gameID, new ArrayList<Integer>() {{
+                add(userID);
+            }});
+        }
     }
 
     @Override public List<Integer> listPlayersInGame(int gameID) throws Exception
     {
-        return null;
+        List<Integer> gameUsers = users.get(gameID);
+        if (gameUsers != null) {
+            return gameUsers;
+        } else {
+            throw new Exception("There are no players in the game to list in game: " + gameID);
+        }
+
     }
 }
