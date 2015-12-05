d7a150f1021ee18e1dfc807c57aaa49e49a70320
Amanda <theshecoder@gmail.com>
2015-12-05 16:17:19 -0700

    User Access Java Serialization Start

--- JavaSerializationEngine/src/UserAccess.java
+++ JavaSerializationEngine/src/UserAccess.java
@@ -2,14 +2,20 @@ import server.manager.User;
 import server.plugin.IUserAccess;
 import shared.communication.Credentials;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by dtaylor on 12/5/2015.
  */
 public class UserAccess implements IUserAccess, IAccess
 {
+    Map<Integer, List<User>> users;
+
     @Override public void initialize()
     {
-
+        users = new HashMap<>();
     }
 
     @Override public User getUser(Credentials credentials) throws Exception
